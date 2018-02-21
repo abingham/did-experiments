463563596a391574ad095ece459e557f29590ec6
Austin Bingham <austin.bingham@gmail.com>
2018-02-21 09:25:05 +0100

    using docker compose per Jon's suggestion

--- client/client.py
+++ client/client.py
@@ -1,16 +1,19 @@
 import asyncio
 from contextlib import contextmanager
-
 import aiohttp
 import docker
+import time
 
 
 @contextmanager
 def using_container(docker_client, *args, **kwargs):
-    container = docker_client.containers.run(*args, **kwargs, detach=True)
+    container = docker_client.containers.run(
+        *args, **kwargs, detach=True)
 
-    while container.status == 'created':
+    while container.status != 'running':
         container.reload()
+
+    time.sleep(2)
 
     try:
         yield container
@@ -22,20 +25,18 @@ def using_container(docker_client, *args, **kwargs):
 
 async def main():
     dclient = docker.from_env()
-
     with using_container(
             dclient,
             image="abingham/aio-server",
             name="aio-server",
-            ports={
-                '4647/tcp': 4647,
-            },
+            network="aio",
+            ports={ '4647/tcp': 4647 },
     ) as container:
-
         async with aiohttp.ClientSession() as session:
-            async with session.get('http://docker.for.mac.host.internal:4647/foo') as resp:
+            async with session.get('http://aio-server:4647/foo') as resp:
                 print(resp.status)
                 print(await resp.text())
+
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
